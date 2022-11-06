3df3d4168512e81b81fa71d5c5dcddad31775091
Ilya Bliznyuk <Entrerry@gmail.com>
2022-11-06 19:21:18 +0300

    set_not_null_to_answers_correct?

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_06_153543) do
+ActiveRecord::Schema.define(version: 2022_11_06_161827) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.integer "question_id", null: false
-    t.boolean "correct?"
+    t.boolean "correct?", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
