d6e24aec91bafece2716adceccbdcd18f0642f70
Ilya Bliznyuk <Entrerry@gmail.com>
2022-11-06 16:33:49 +0300

    fix_user_and_categorie_model

--- db/migrate/20221106133052_create_categories.rb
+++ db/migrate/20221106133052_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
       t.string :body
-      t.string :test_id
+      t.integer :test_id
 
       t.timestamps
     end
