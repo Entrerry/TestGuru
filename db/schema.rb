9faf72b99394ef3742e3a189f26d3636b687ec96
Ilya Bliznyuk <Entrerry@gmail.com>
2022-11-06 19:39:04 +0300

    set_not_null_to_users_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_06_163353) do
+ActiveRecord::Schema.define(version: 2022_11_06_163739) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2022_11_06_163353) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
