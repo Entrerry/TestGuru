514fb81287106eb9d12a4bac12febd8ee15477ce
Ilya Bliznyuk <Entrerry@gmail.com>
2022-11-07 21:30:21 +0300

    fix_category

--- db/schema.rb
+++ db/schema.rb
@@ -38,10 +38,10 @@ ActiveRecord::Schema.define(version: 2022_11_06_133052) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
-    t.integer "categorie_id", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["categorie_id"], name: "index_tests_on_categorie_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,5 +52,5 @@ ActiveRecord::Schema.define(version: 2022_11_06_133052) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "tests", "categories", column: "categorie_id"
+  add_foreign_key "tests", "categories"
 end
